558ede338b16aa7ec6b56e6fb93f6a6cbb289c52
omrylcn <omeryalcin48@gmail.com>
2025-01-10 03:25:11 +0300

    Edit worker  for config file

--- worker.py
+++ worker.py
@@ -4,17 +4,25 @@ from src.core.config import settings
 from src.ml.style_transfer_service import ImageService
 
 
-model_name = os.getenv('MODEL_NAME', 'candy')
-model_number = int(os.getenv('MODEL_NUMBER', '0'))
-model_path = os.getenv('MODEL_PATH', 'models/candy-8.onnx')
+model_key = os.getenv('MODEL_KEY', None)
 
+if model_key is None:
+    raise ValueError("model key environment variable is not set")
 
+try:
+    model_config = settings.ml_settings.models[model_key]
+    model_name = model_config.name
+    model_version = model_config.version
+    model_number = model_config.model_id
+    model_path = model_config.path
 
+except KeyError:
+    raise KeyError(f"Model {model_key} not found in ML config")
+except Exception as e:
+    raise Exception(f"Error loading ML config: {str(e)}")
 
-style_transfer_service = ImageService(model_path=model_path)
-model_instance = {}
-worker_task, celery_app = create_worker_task(settings,"candy",0,style_transfer_service)
-
-
-
-
+try:
+    style_transfer_service = ImageService(model_path=model_path)
+    worker_task, celery_app = create_worker_task(settings, model_name, style_transfer_service)
+except Exception as e:
+    raise Exception(f"Error initializing services: {str(e)}")
